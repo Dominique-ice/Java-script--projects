65a8b534d6a44d6d65dffd0188e6c6d24514714f
Dominique-ice <nwankwos863@gmail.com>
2026-04-12 13:20:26 +0100

    "Add story generator project"

---
+++ Storybot.js
@@ -0,0 +1,20 @@
+let adjective = "beautiful";
+let noun = "girl";
+let verb = "hurting";
+let place = "village";
+let adjective2 = "small";
+let noun2 = "rice";
+
+let firstStory = "Once upon a time, there was a " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("first story:", firstStory);
+
+// New values
+adjective = "fair";
+noun = "boy";
+verb = "sweating";
+place = "cabin";
+adjective2 = "wide";
+noun2 = "granola";
+
+let secondStory = "Once upon a time, there was a " + adjective + " " + noun + " who loves to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("second story:", secondStory);
