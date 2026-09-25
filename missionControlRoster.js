0e6916bbc8f72dd0e8c45408334ecaaf2ce71cc5
Samuel Nwankwo <nwankwos863@gmail.com>
2026-09-25 11:22:55 +0100

    feat: add mission control crew roster system

---
+++ missionControlRoster.js
@@ -0,0 +1,70 @@
+function addCrewMember(crew, astronaut) {
+  for (let i = 0; i < crew.length; i++) {
+    if (crew[i].id === astronaut.id) {
+      console.log("Duplicate ID: " + astronaut.id);
+      return;
+    }
+  }
+  crew.push(astronaut);
+}
+
+function swapCrewMembers(crew, fromIndex, toIndex) {
+  if (
+    fromIndex < 0 ||
+    toIndex < 0 ||
+    fromIndex >= crew.length ||
+    toIndex >= crew.length
+  ) {
+    console.log("Invalid crew indices");
+    return;
+  }
+
+  const updatedCrew = crew.slice();
+  updatedCrew[fromIndex] = updatedCrew.splice(toIndex, 1, updatedCrew[fromIndex])[0];
+
+  return updatedCrew;
+}
+
+function sortByPriorityDescending(crew) {
+  for (let i = 0; i < crew.length - 1; i++) {
+    for (let j = 0; j < crew.length - 1 - i; j++) {
+      if (crew[j].priority < crew[j + 1].priority) {
+        const temp = crew[j];
+        crew[j] = crew[j + 1];
+        crew[j + 1] = temp;
+      }
+    }
+  }
+}
+
+function getEVAReadyCrew(crew) {
+  const eligible = [];
+  for (const astronaut of crew) {
+    if (astronaut.isEVAEligible) eligible.push(astronaut);
+  }
+  sortByPriorityDescending(eligible);
+
+  return eligible;
+}
+
+function chunkCrew(crew, size) {
+  if (size < 1) {
+    console.log("Chunk size must be >= 1");
+    return;
+  }
+
+  const chunks = [];
+  for (let i = 0; i < crew.length; i += size) {
+    chunks.push(crew.slice(i, i + size));
+  }
+
+  return chunks;
+}
+
+function printCrewSummary(crew) {
+  const sorted = crew.slice();
+  sortByPriorityDescending(sorted);
+  for (const astronaut of sorted) {
+    console.log(astronaut.name);
+  }
+    }
